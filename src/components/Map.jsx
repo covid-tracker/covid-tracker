c998d94c853ca35c64ba2613b3ec467f32fe20bd
keil <keilwmartin@gmail.com>
2020-04-21 14:51:27 -0400

    up to date version

---
+++ src/components/Map.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+const style = {
+  padding: "3px 10px",
+  color: "#fff",
+  cursor: "pointer",
+  background: "#111",
+  borderRadius: "50px",
+};
+class Map extends Component {
+  state = {
+    viewport: {
+      width: 900,
+      height: 500,
+      latitude: 53.76,
+      longitude: -98.81,
+      zoom: 2.6,
+    },
+  };
+  render() {
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        mapboxApiAccessToken={
+          "pk.eyJ1IjoieGVub2pheCIsImEiOiJjazk5MDlpNDEwN2o2M21ueDVoNXQ1eWpnIn0.-Sy_LkU_ZYcmyH09Zl-MYw"
+        }
+        onViewportChange={(viewport) => this.setState({ viewport })}
+      >
+        {this.props.markerData.map((province) => {
+          return (
+            <Marker
+              latitude={parseInt(province.Lat)}
+              longitude={parseInt(province.Lon)}
+            >
+              <div style={style}>
+                {province.Province} - {province.Cases}
+              </div>
+            </Marker>
+          );
+        })}
+      </ReactMapGL>
+    );
+  }
+}
+export default Map;
