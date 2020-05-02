1032270509126d7eae30f154ff07fe2c38c99ccd
keil <keilwmartin@gmail.com>
2020-05-02 00:17:17 -0400

    reverted back, issues reverted

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,23 +1,5 @@
 import React, { Component } from "react";
 
-// render() {
-//   console.log(this.props.widgetData);
-//   {this.props.widgetData.map((mainStats, index) => {
-//   return (
-//     <div>
-//       <h1>Hello</h1>
-//       <button
-//         onClick={() => this.props.widgetData(mainStats)}
-//         key={mainStats.Slug}
-//       >
-//         {mainStats.TotalConfirmed}
-//       </button>
-//     </div>
-//   );
-//     })}
-//   );
-// }
-
 class Widget extends Component {
   render() {
     console.log(this.props.widgetData);
@@ -26,32 +8,8 @@ class Widget extends Component {
         <div className="box container" style={{ textAlign: "center" }}>
           <header>
             <h1 className="title" style={{ textAlign: "center" }}>
-              Widgets
+              Widget
             </h1>
-            {/* {this.props.widgetData.map((singleProvince, index) => {
-                  return (
-                    // <tbody>
-                    //   <tr key={index}>
-                    //     <td
-                    //       onClick={() =>
-                    //         this.props.clickEventForGraph(singleProvince)
-                    //       }
-                    //       key={singleProvince.Lon}
-                    //     >
-                    //       {singleProvince.Province}
-                    //     </td>
-                    //     <td
-                    //       onClick={() =>
-                    //         this.props.clickEventForGraph(singleProvince)
-                    //       }
-                    //       key={singleProvince.Lat}
-                    //     >
-                    //       {singleProvince.Cases}
-                    //     </td>
-                    //   </tr>
-                    // </tbody>
-                  );
-                })} */}
           </header>
         </div>
       </section>
