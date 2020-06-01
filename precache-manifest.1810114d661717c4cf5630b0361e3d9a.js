87de2b06713f3333a78276859389d2646f1409a1
Vipin <kirthane.vipin@gmail.com>
2020-05-31 22:16:04 -0400

    Updates

---
+++ precache-manifest.1810114d661717c4cf5630b0361e3d9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c244b14b79499a231cccf0c65a966de",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "8e8855e0e2eb968d4cfa",
+    "url": "/covid-tracker/static/css/main.36111bf1.chunk.css"
+  },
+  {
+    "revision": "6ec88f144f09d43606e4",
+    "url": "/covid-tracker/static/js/2.e9a65c47.chunk.js"
+  },
+  {
+    "revision": "1fbc5163fc4198124faa18b29c1957e4",
+    "url": "/covid-tracker/static/js/2.e9a65c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e8855e0e2eb968d4cfa",
+    "url": "/covid-tracker/static/js/main.c0bb4d26.chunk.js"
+  },
+  {
+    "revision": "912f0c7427d740d7fce2",
+    "url": "/covid-tracker/static/js/runtime-main.5cf94d1e.js"
+  }
+]);
