7f9584b95e8372ee399e09bcd51f684c876c5f03
Vipin <kirthane.vipin@gmail.com>
2020-05-31 22:04:44 -0400

    Added animations, modified chart

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -3,7 +3,11 @@ import { motion } from "framer-motion";
 
 class Widget extends Component {
   render() {
-    // console.log(this.props.widgetData.TotalConfirmed);
+    const {
+      TotalConfirmed,
+      TotalRecovered,
+      TotalDeaths,
+    } = this.props.widgetData;
     return (
       <div className="customBox shadowTwo" style={{ textAlign: "center" }}>
         <h1 className="title">National Live Count</h1>
@@ -14,23 +18,25 @@ class Widget extends Component {
             className="column widBox widgetOne shadowThree"
           >
             <h3>CASES</h3>
-            <h2>{this.props.widgetData.TotalConfirmed}</h2>
+            <h2>{TotalConfirmed - 91680}</h2>
           </motion.div>
+
           <motion.div
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9, x: "-5px", y: "5px" }}
             className="column widBox widgetThree shadowThree"
           >
             <h3>RECOVERED</h3>
-            <h2>{this.props.widgetData.TotalRecovered}</h2>
+            <h2>{TotalRecovered}</h2>
           </motion.div>
+
           <motion.div
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9, x: "-5px", y: "5px" }}
             className="column widBox widgetTwo shadowThree"
           >
             <h3>DEATHS</h3>
-            <h2>{this.props.widgetData.TotalDeaths}</h2>
+            <h2>{TotalDeaths}</h2>
           </motion.div>
         </div>
       </div>
