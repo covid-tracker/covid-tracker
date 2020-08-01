1945c110ad4b10d7ab940da9c450989e2efad074
keil <keilwmartin@gmail.com>
2020-08-01 14:44:58 -0400

    added default line graph data and made responsive

--- src/components/LineGraph.jsx
+++ src/components/LineGraph.jsx
@@ -6,36 +6,71 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
+  ResponsiveContainer,
 } from "recharts";
+
+let data = [
+  {
+    Date: 'Jan 1', Cases: 100,
+  },
+  {
+    Date: 'Feb 1', Cases: 500,
+  },
+  {
+    Date: 'Mar 1', Cases: 300,
+  },
+  {
+    Date: 'Apr 1', Cases: 800,
+  },
+  {
+    Date: 'May 1', Cases: 500,
+  },
+  {
+    Date: 'Jun 1', Cases: 1300,
+  },
+  {
+    Date: 'Jul 1', Cases: 1200,
+  },
+  {
+    Date: 'Auq 1', Cases: 2100,
+  },
+];
+
+
+
+
 
 class LineGraph extends Component {
   render() {
     const { lineGraphFinalFunction } = this.props;
+    console.log(this.props)
     return (
       <div className="customBox shadowTwo">
         <h3 style={{ textAlign: "center", color: "white", fontSize: 15 }}>
           Province Cases
         </h3>
-        <LineChart
-          width={490}
-          height={422}
-          data={lineGraphFinalFunction}
-          margin={{ top: 5, right: 10, left: 0, bottom: 5 }}
-          style={{ margin: "0 auto" }}
-        >
-          <CartesianGrid strokeDasharray="1 1" />
-          <XAxis dataKey="Date" stroke="#f35163" />
-          <YAxis stroke="#f35163" />
-          <Tooltip />
-          <Line
-            dot={false}
-            animationDuration={3000}
-            type="monotone"
-            dataKey="Cases"
-            stroke="#4f7cff"
-            strokeWidth="6"
-          />
-        </LineChart>
+        <ResponsiveContainer width='100%' aspect={3.5 / 3.0}>
+          <LineChart
+            width={490}
+            height={422}
+            data={data}
+            margin={{ top: 5, right: 10, left: 0, bottom: 5 }}
+            style={{ margin: "0 auto" }}
+          >
+            <CartesianGrid strokeDasharray="1 1" />
+            <XAxis dataKey="Date" stroke="#f35163" />
+            <YAxis stroke="#f35163" />
+            <Tooltip />
+            <Line
+              dot={false}
+              animationDuration={3000}
+              type="monotone"
+              dataKey="Cases"
+              stroke="#4f7cff"
+              strokeWidth="6"
+            />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     );
   }
