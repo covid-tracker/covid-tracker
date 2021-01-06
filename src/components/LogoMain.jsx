4bfdfe1a081e449e3a5269b2b97f5a65781d9969
keil <keilwmartin@gmail.com>
2021-01-06 15:54:54 -0500

    added error window due to api not working

--- src/components/LogoMain.jsx
+++ src/components/LogoMain.jsx
@@ -10,7 +10,7 @@ class LogoMain extends Component {
           </h1>
           <p className="subtitle">Provincial Case Numbers</p>
           <button className="alertButton"
-            onClick={(e) => window.alert('covid19api.com went down on January 1st, Their dev team is working on a fix, Update coming soon, check back!')}>
+            onClick={(e) => window.alert('covid19api.com went down on January 1st due to the year changing, Their dev team is working on a fix, Update coming soon, check back!')}>
             Important! - Click me first!
           </button>
         </div>
