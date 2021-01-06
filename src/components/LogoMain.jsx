ad485a8498986531dbe66467eaf95bf15922d345
keil <keilwmartin@gmail.com>
2021-01-06 13:28:01 -0500

    added covid button

--- src/components/LogoMain.jsx
+++ src/components/LogoMain.jsx
@@ -9,6 +9,10 @@ class LogoMain extends Component {
             Canada COVID-19 Tracker
           </h1>
           <p className="subtitle">Provincial Case Numbers</p>
+          <button className="alertButton"
+            onClick={(e) => window.alert('covid19api.com went down on January 1st, Their dev team is working on a fix, Update coming soon, check back!')}>
+            Important! - Click me first!
+          </button>
         </div>
       </section>
     );
